b32817023ac4ffc79de2da39715aef6b4ba748e8
zawwaizoe <rohinjar456+1@gmail.com>
2018-01-15 05:47:05 -0800

    Everything is fine

---
+++ app/src/main/java/com/example/kryptonite/mygithub/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kryptonite.mygithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1,btn2;
+    private EditText et1;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
