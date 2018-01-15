2c5f24e49176af9e396ecbe5883997912392e7bc
zawwaizoe <rohinjar456+1@gmail.com>
2018-01-15 05:59:01 -0800

    Two Text View added

--- app/src/main/java/com/example/kryptonite/mygithub/MainActivity.java
+++ app/src/main/java/com/example/kryptonite/mygithub/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn1,btn2;
     private EditText et1;
-    private TextView tv1;
+    private TextView tv1,tv2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
