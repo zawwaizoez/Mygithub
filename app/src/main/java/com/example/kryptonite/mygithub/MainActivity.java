cf2e9489203594c30ad375f94f9b4da4e05fe361
zawwaizoe <rohinjar456+1@gmail.com>
2018-01-15 05:57:14 -0800

    TextView added

--- app/src/main/java/com/example/kryptonite/mygithub/MainActivity.java
+++ app/src/main/java/com/example/kryptonite/mygithub/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn1,btn2;
     private EditText et1;
+    private TextView tv1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
